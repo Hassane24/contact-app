ffa108040fc5bdd17822ed2641096d3e2a818782
Hassane <hsisane5@gmail.com>
2025-04-29 16:05:17 +0100

    switched to expo sqlite

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,38 +1,35 @@
-import {
-  enablePromise,
-  openDatabase,
-  SQLiteDatabase,
-} from "react-native-sqlite-storage";
+import * as SQLite from "expo-sqlite";
 
-// Enable promise for SQLite
-enablePromise(true);
+import { Contact } from "../components/contacts/Contacts";
 
-const connectToDatabase = async () => {
-  return openDatabase(
-    { name: "yourProjectName.db", location: "default" },
-    () => {},
-    (error) => {
-      console.error(error);
-      throw Error("Could not connect to database");
-    }
-  );
-};
+export const DUMMY_CONTACTS: Contact[] = [
+  {
+    id: "1",
+    name: "Takeshi Movic",
+    location: "San Francisco, CA",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+  },
+  {
+    id: "2",
+    name: "Emma Larwind",
+    location: "Stockholm, SE",
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    id: "3",
+    name: "Giovanni Naitila",
+    location: "Rome, Italy",
+    image: "https://randomuser.me/api/portraits/men/3.jpg",
+  },
+  {
+    id: "4",
+    name: "Miyoshi Zawn",
+    location: "Los Angeles, CA",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+];
 
-const createTables = async (db: SQLiteDatabase) => {
-  const contactsQuery = `
-   CREATE TABLE IF NOT EXISTS Contacts (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      firstName TEXT,
-      name TEXT,
-      phoneNumber TEXT
-   )
-  `;
-  try {
-    await db.executeSql(contactsQuery);
-  } catch (error) {
-    console.error(error);
-    throw Error(`Failed to create tables`);
-  }
-};
+const connectToDatabase = async () =>
+  await SQLite.openDatabaseAsync("databaseName");
 
-export default { connectToDatabase, createTables };
+export default connectToDatabase;
